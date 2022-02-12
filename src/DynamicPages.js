c627a961db6811981c7097cf4ce02f145fadc4eb
ali raza <ali.raza@justads.com>
2022-02-12 23:05:55 +0500

    fix

--- src/DynamicPages.js
+++ src/DynamicPages.js
@@ -3,6 +3,9 @@ import { Box } from "@material-ui/core";
 import Drawer from "@mui/material/Drawer";
 import ListSubheader from "@mui/material/ListSubheader";
 import ListItemButton from "@mui/material/ListItemButton";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
 // import Card from '@mui/material/Card';
 // import CardActions from '@mui/material/CardActions';
 // import CardContent from '@mui/material/CardContent';
@@ -18,7 +21,14 @@ import InputBase from "@mui/material/InputBase";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import LaptopMacIcon from "@mui/icons-material/LaptopMac";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from '@mui/material/TableRow';
 import LogoutIcon from "@mui/icons-material/Logout";
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
 // import TableRow from '@mui/material/TableRow';
 // import DynamicPage from "./DynamicPages";
 // import  { Redirect } from 'react-router-dom'
@@ -42,88 +52,142 @@ import { useNavigate } from "react-router-dom";
 // import withRouter from "react-router-dom/es/withRouter";
 import axios from "axios";
 
+
 const drawerWidth = 240;
-const useStyles = makeStyles({
-  drawer: {
-    width: drawerWidth,
-    borderColor: "slateblue",
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    boxSizing: "border-box",
-    borderColor: "slateblue",
-  },
-  active: {
-    background: "slateblue",
-  },
-  title: {
-    fontFamily: "serif",
-  },
-  uptoolbar: {
-    height: 10,
-    width: -300,
-  },
-  application: {
-    right: -237,
-    top: -0,
-    position: "relative",
-  },
-  search: {
-    left: 980,
-  },
+const useStyles=makeStyles({
+    drawer:{
+        width: drawerWidth,
+        borderColor:"slateblue",
+    },
+    drawerPaper:{
 
-  searchmain: {
-    border: "solid 2px",
-    borderColor: "slateblue",
-    left: 550,
-    top: 180,
-    position: "absolute",
-    maxWidth: 550,
-  },
+        width: drawerWidth,
+        boxSizing: 'border-box',
+        borderColor:"slateblue",
 
-  font: {
-    fontFamily: "serif",
-    color: "slateblue",
-  },
-  card: {
-    position: "relative",
-    left: 250,
-    top: 300,
-  },
-  app: {
-    right: -240,
-    position: "relative",
-  },
-  media: {
-    position: "relative",
-    color: "slateblue",
-    fontFamily: "serif",
-    display: "block",
-    whiteSpace: "unset",
-    width: 100,
-    overflow: "hidden",
-    //textOverflow:"clip",
-    border: 1,
-  },
-  mediaImage: {
-    position: "relative",
-    color: "slateblue",
-    fontFamily: "sans-serif",
-    left: 50,
-    top: -45,
-  },
-  titile: {
-    overflow: "hidden",
-    left: 650,
-    top: 160,
-    position: "absolute",
-    fontFamily: "serif",
-    fontSize: 54,
-  },
-  listFont: {
-    fontSize: 14,
-  },
-});
+    },
+    active:{
+        background:"slateblue",
+    },
+    title:{
+        fontFamily:"serif",
+    },
+    uptoolbar:{
+        height:10,
+        width:-300
+
+    },
+    application:{
+        // right:-237,
+        top:-0,
+        position:"absolute"
+
+    },
+    search:{
+        left: 980,
+    },
+
+    searchmain:{
+        border:"solid 2px",
+        borderColor:"slateblue",
+        left:550,
+        top:180,
+        position:"absolute",
+        maxWidth:550,
+    },
+
+    font:{
+        fontFamily:"serif",
+        color:"slateblue",
+    },
+    card:{
+        position:"relative",
+        left:250,
+        top:300,
+    },
+    app:{
+        right:-240,
+        position:"relative"
+    },
+    media:{
+        position:"relative",
+        color: "slateblue",
+        fontFamily:"serif",
+        display: "block",
+        whiteSpace:"unset",
+        width:100,
+        overflow:"hidden",
+        //textOverflow:"clip",
+        border:1,
+    },
+    mediaImage:{
+        position:"absolute",
+        color: "slateblue",
+        fontFamily:"sans-serif",
+        top:20,
+        left:-0,
+
+    },
+    titile:
+        { overflow:"hidden",
+            left:650,
+            top:160,
+            position:"absolute",
+            fontFamily:"serif",
+            fontSize:54
+        },
+    listFont:{
+        fontSize:14,
+    },
+    productname:{
+        position:"absolute",
+        top:120,
+        left:590,
+        fontSize:31,
+        fontFamily:"serif",
+        color:"slateblue",
+        display: "block",
+        whiteSpace:"unset",
+        width:700,
+        overflow:"hidden",
+        //textOverflow:"clip",
+        border:1,
+    },
+    productdesc:{
+        position:"absolute",
+        top:190,
+        left:500,
+        fontSize:20,
+        fontFamily:"serif",
+        color:"slateblue",
+        display: "block",
+        whiteSpace:"unset",
+        width:500,
+        overflow:"hidden",
+        textOverflow:"clip",
+        border:1,
+    },
+    imageCard:{
+        position:"absolute",
+        width:300,
+        left:278,
+        top:110,
+        height:350,
+    },
+    list:{
+        right:340,
+        top:490,
+        position:"absolute",
+        width:660
+    },
+    tableheading:{
+        color:"slateblue",
+        fontSize:16,
+        fontWeight:"bold"
+    }
+
+})
+
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -170,6 +234,8 @@ function ProductDetails(props) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [data, setData] = useState({});
+  const [price, setPrice] = useState([]);
+
   let { id } = useParams();
   let history = useNavigate();
 
@@ -188,6 +254,24 @@ function ProductDetails(props) {
         console.log("this is the error: ", { err });
       });
   }, []);
+
+
+  useEffect(() => {
+
+    axios({
+      url: `https://finalproject-helpmesell.herokuapp.com/api/price/`,
+      method: "GET",
+    })
+      .then(({ data }) => {
+        console.log("this is the data of a single product: ", { data });
+        setPrice(data.filter( d => d.product ==  id));
+      })
+      .catch((err) => {
+        console.log("this is the error: ", { err });
+      });
+  }, []);
+
+
   function handleClick() {
     setOpen(!open);
   }
@@ -201,99 +285,105 @@ function ProductDetails(props) {
     <div>
       {data !== {} ? (
         <>
-          <Typography
-            variant="h1"
-            style={{ left: 300, top: 300, position: "absolute" }}
-          >
-            {/* Hello{props.match.params.id} */}
-            Hello{data.id}
-          </Typography>
-          <Typography style={{ left: 300, top: 300, position: "absolute" }}>
-            Name:{data.product_name}
-          </Typography>
+         <TableContainer component={Paper}>*/}
+                <Table className={classes.list} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell className={classes.tableheading} align="left">Store</TableCell>
+                            <TableCell className={classes.tableheading} align="right">Price (Rs)</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {price.map((row) => (
+                           <TableRow key={row.id} >
+                              <TableCell align="right">{row.reference_site}</TableCell>
+                              <TableCell align="right">{row.product_price}</TableCell>
+                           </TableRow>
+                        ))}
 
-          <Box sx={{ display: "flex" }}>
-            <Box sx={{ flexGrow: 5 }}>
-              <AppBar position="static" className={classes.application}>
-                <Toolbar></Toolbar>
-              </AppBar>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            <Typography className={classes.productname}>{data.product_name} </Typography>
+
+            <Card className={classes.imageCard}>
+                <CardContent>
+                    <Typography align="left" className={classes.mediaImage}>{<img style={{width: 300}} src={data.product_image}/>}</Typography>
+                </CardContent>
+            </Card>
+
+
+            <Box sx={{ display: 'flex' }}>
+                <Box sx={{ flexGrow: 5 }}>
+                    <AppBar position="static" className={classes.application}>
+                        <Toolbar>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
+                <Drawer className={classes.drawer} variant="permanent" anchor="left"
+                        classes={{paper:classes.drawerPaper}}>
+                    <AppBar position="static">
+                        <Toolbar  className={classes.uptoolbar}>
+                            <Typography variant="h6" className={classes.title}>
+                                HelpMeSell
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                    <List
+                        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+                        component="nav"
+                        aria-labelledby="nested-list-subheader"
+                        subheader={
+                            <ListSubheader component="div" id="nested-list-subheader">
+                            </ListSubheader>
+                        }
+                    >
+                        <ListItemButton component={Link} to="/marketsurvey">
+                            <ListItemIcon>
+                                <EqualizerIcon color={"primary"} />
+                            </ListItemIcon>
+                            <ListItemText primary="Market Survey" />
+                        </ListItemButton>
+                        <ListItemButton component={Link} to="/pricecomparison">
+                            <ListItemIcon>
+                                <PaidRoundedIcon  color={"primary"}/>
+                            </ListItemIcon>
+                            <ListItemText primary="Price Comparison"/>
+                        </ListItemButton>
+                        <ListItemButton onClick={handleClick}component={Link} to="/homepage" >
+                            <ListItemIcon>
+                                <AppsIcon color={"primary"}/>
+                            </ListItemIcon>
+                            <ListItemText primary="Category" />
+                            {open ? <ExpandLess /> : <ExpandMore />}
+                        </ListItemButton>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                <ListItemButton sx={{ pl: 4 }}>
+                                    <ListItemIcon>
+                                        <PhoneIphoneIcon color={"primary"}/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Smartphones" />
+                                </ListItemButton>
+                                <ListItemButton sx={{ pl: 4 }}>
+                                    <ListItemIcon>
+                                        <LaptopMacIcon color={"primary"}/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Laptops" />
+                                </ListItemButton>
+                            </List>
+                        </Collapse>
+                        <ListItemButton component={Link} to="/">
+                            <ListItemIcon>
+                                <LogoutIcon color={"primary"} />
+                            </ListItemIcon>
+                            <ListItemText primary="Logout" />
+                        </ListItemButton>
+                    </List>
+                </Drawer>
             </Box>
-            <Drawer
-              className={classes.drawer}
-              variant="permanent"
-              anchor="left"
-              classes={{ paper: classes.drawerPaper }}
-            >
-              <AppBar position="static">
-                <Toolbar className={classes.uptoolbar}>
-                  <Typography variant="h6" className={classes.title}>
-                    HelpMeSell
-                  </Typography>
-                </Toolbar>
-              </AppBar>
-              <List
-                sx={{
-                  width: "100%",
-                  maxWidth: 360,
-                  bgcolor: "background.paper",
-                }}
-                component="nav"
-                aria-labelledby="nested-list-subheader"
-                subheader={
-                  <ListSubheader
-                    component="div"
-                    id="nested-list-subheader"
-                  ></ListSubheader>
-                }
-              >
-                <ListItemButton component={Link} to="/marketsurvey">
-                  <ListItemIcon>
-                    <EqualizerIcon color={"primary"} />
-                  </ListItemIcon>
-                  <ListItemText primary="Market Survey" />
-                </ListItemButton>
-                <ListItemButton component={Link} to="/pricecomparison">
-                  <ListItemIcon>
-                    <PaidRoundedIcon color={"primary"} />
-                  </ListItemIcon>
-                  <ListItemText primary="Price Comparison" />
-                </ListItemButton>
-                <ListItemButton
-                  onClick={handleClick}
-                  component={Link}
-                  to="/homepage"
-                >
-                  <ListItemIcon>
-                    <AppsIcon color={"primary"} />
-                  </ListItemIcon>
-                  <ListItemText primary="Category" />
-                  {open ? <ExpandLess /> : <ExpandMore />}
-                </ListItemButton>
-                <Collapse in={open} timeout="auto" unmountOnExit>
-                  <List component="div" disablePadding>
-                    <ListItemButton sx={{ pl: 4 }}>
-                      <ListItemIcon>
-                        <PhoneIphoneIcon color={"primary"} />
-                      </ListItemIcon>
-                      <ListItemText primary="Smartphones" />
-                    </ListItemButton>
-                    <ListItemButton sx={{ pl: 4 }}>
-                      <ListItemIcon>
-                        <LaptopMacIcon color={"primary"} />
-                      </ListItemIcon>
-                      <ListItemText primary="Laptops" />
-                    </ListItemButton>
-                  </List>
-                </Collapse>
-                <ListItemButton component={Link} to="/">
-                  <ListItemIcon>
-                    <LogoutIcon color={"primary"} />
-                  </ListItemIcon>
-                  <ListItemText primary="Logout" onClick={logoutHandler} />
-                </ListItemButton>
-              </List>
-            </Drawer>
-          </Box>
+
         </>
       ) : (
         <h5>Loading</h5>
